9699c27225338601bb7fda5696951a8a195df8e4
furupon <furupon2008@gmail.com>
2020-05-27 19:56:06 +0900

    Initial Commit

---
+++ Landmarks２回目/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks２回目
+//
+//  Created by 古川とおる on 2020/05/27.
+//  Copyright © 2020 古川とおる. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
